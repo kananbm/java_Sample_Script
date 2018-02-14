5e06509993c7dfd8f23c59b293b836a6ac7cf013
kananbm <35985722+kananbm@users.noreply.github.com>
2018-02-14 15:15:04 +0530

    Update spec.ts

--- tests/spec.ts
+++ tests/spec.ts
@@ -5,9 +5,9 @@ import {SampleApplicationPage} from './sampleApplication.po';
 describe('Sample Application homepage', () => {
     let page: SampleApplicationPage;
 
-    let imageSrc: string = "people.png";
+    //let imageSrc: string = "people.png";
     let title: string = "CA Continuous Delivery Director";
-    let message: string = "You deployed the sample application. Next thing to do is run some tests. Follow the 'Getting Started' to see how.";
+    //let message: string = "You deployed the sample application. Next thing to do is run some tests. Follow the 'Getting Started' to see how.";
 
     beforeAll(() => {
         browser.ignoreSynchronization = true;
@@ -15,17 +15,17 @@ describe('Sample Application homepage', () => {
         return page.get();
     });
 
-    it('test top image validness', () => {
-        expect(page.getImageSrc()).toContain(imageSrc);
-    });
+    //it('test top image validness', () => {
+    //    expect(page.getImageSrc()).toContain(imageSrc);
+    //});
 
     it('test title validness', () => {
         expect(page.getTitle()).toMatch(title);
     });
 
-    it('test message validness', () => {
-        expect(page.getMessage()).toMatch(message);
-    });
+    //it('test message validness', () => {
+    //    expect(page.getMessage()).toMatch(message);
+    //});
 
     afterAll(() => {
         browser.ignoreSynchronization = false;
