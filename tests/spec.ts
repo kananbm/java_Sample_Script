1ab16afcbd07bf2299048cfe42855be972c0eafc
kananbm <35985722+kananbm@users.noreply.github.com>
2018-02-15 18:27:08 +0530

    Update spec.ts

--- tests/spec.ts
+++ tests/spec.ts
@@ -5,25 +5,25 @@ import {SampleApplicationPage} from './sampleApplication.po';
 describe('Sample Application homepage', () => {
     let page: SampleApplicationPage;
 
-      let imageSrc: string = "https://demo.cyclos.org/content/images/currentConfiguration/SYSTEM_LOGO?mod=1498899439344";
+   //   let imageSrc: string = "https://demo.cyclos.org/content/images/currentConfiguration/SYSTEM_LOGO?mod=1498899439344";
   //  let title: string = "Mock Trading - Online Futures Trading Simulator";
-  //  let message: string = "Kanna";
+    let message: string = "CA Continuous Delivery Director";
 
     beforeAll(() => {
         browser.ignoreSynchronization = true;
         page = new SampleApplicationPage();
         return page.get();
     });
-
+/*
    it('test top image validness', () => {
         expect(page.getImageSrc()).toContain(imageSrc);
     });
-   /* 
+   
 
     it('test title validness', () => {
         expect(page.getTitle()).toMatch(title);
     });
-
+*/
     it('test message validness', () => {
         expect(page.getMessage()).toMatch(message);
     });
@@ -31,5 +31,5 @@ describe('Sample Application homepage', () => {
     afterAll(() => {
         browser.ignoreSynchronization = false;
     });
-    */
+    
 });
