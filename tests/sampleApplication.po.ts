dc7eb74aab66fce88e5f000566b88a0af4fffef9
kananbm <35985722+kananbm@users.noreply.github.com>
2018-02-15 17:28:24 +0530

    Update sampleApplication.po.ts

--- tests/sampleApplication.po.ts
+++ tests/sampleApplication.po.ts
@@ -6,16 +6,14 @@ import {TEST_URL} from "./test.config";
 
 export class SampleApplicationPage {
     // nameInput = element(by.model('yourName'));
-  //  image = element(by.tagName('img'));
+    image = element(by.tagName('img'));
   //  title = element(by.tagName('title'));
    // message = element(by.tagName('h5'));
-    
-    message = element(by.Xpath('//*[@class="inputField medium"'));
-   
+  
     get() {
         browser.get(TEST_URL);
     }
-/*
+
     getImage(): ElementFinder {
         return this.image;
     }
@@ -23,13 +21,13 @@ export class SampleApplicationPage {
     getImageSrc(): Promise<string> {
         return this.getImage().getAttribute('src');
     }
-
+/*
     getTitle(): Promise<string> {
         return this.title.getText();
     }
-*/
+
     getMessage(): Promise<string> {
         return this.message.getText();
     }
-   
+ */  
 }
