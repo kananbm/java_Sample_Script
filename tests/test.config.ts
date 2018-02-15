22ca76176bd761e71a2efb8c9681aebe18b2b25e
kananbm <35985722+kananbm@users.noreply.github.com>
2018-02-15 16:43:57 +0530

    Update test.config.ts

--- tests/test.config.ts
+++ tests/test.config.ts
@@ -1,3 +1,3 @@
 
 // you shoulc change the TEST_URL to point to the target environment you want to test;
-export const TEST_URL: string = "https://demo.cyclos.org/";
+export const TEST_URL: string = "http://www.mocktrading.com/";
