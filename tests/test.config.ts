257ec646c327dfbdc2647ce0f724e9c73f233238
kananbm <35985722+kananbm@users.noreply.github.com>
2018-02-26 14:05:43 +0530

    Update test.config.ts

--- tests/test.config.ts
+++ tests/test.config.ts
@@ -1,3 +1,3 @@
 
 // you shoulc change the TEST_URL to point to the target environment you want to test;
-export const TEST_URL: string = "D:/kannan/test.html";
+export const TEST_URL: string = "http://ec2-13-126-161-148.ap-south-1.compute.amazonaws.com/";
